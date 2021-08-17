7538e19da6a501e93da11a8457eba2fa5b0fcc27
Paul Nodet <paul.nodet@gmail.com>
2021-08-17 12:16:00 +0200

    Add some func helper functions

---
+++ src/_func.js
@@ -0,0 +1,18 @@
+/**
+ * Invokes `func` after `wait` milliseconds. Any additional arguments are
+ * provided to `func` when it's invoked.
+ * @param {Function} func The function to delay.
+ * @param {number} wait The number of milliseconds to delay invocation.
+ * @param {...*} [args] The arguments to invoke `func` with.
+ * @returns {number} Returns the timer id.
+ * @example
+ *
+ * delay(text => console.log(text), 1000, 'later')
+ * // => Logs 'later' after one second.
+ */
+export function delay(func, wait, ...args) {
+  if (typeof func !== "function") {
+    throw new TypeError("Expected a function");
+  }
+  return setTimeout(func, +wait || 0, ...args);
+}
