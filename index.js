7c16227c90c978b920fbfab6039509b02000cad1
Paul Nodet <paul.nodet@gmail.com>
2021-08-17 12:15:29 +0200

    Update export to use file name directly

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-import * as _Array from "./src/_array.js";
-import * as _Crypto from "./src/_crypto.js";
-import * as _Css from "./src/_css.js";
-import * as _Dom from "./src/_dom.js";
-import * as _Error from "./src/_error.js";
-import * as _Fetch from "./src/_fetch.js";
-import * as _Is from "./src/_is.js";
-import * as _Number from "./src/_number.js";
-import * as _Object from "./src/_object.js";
-import * as _String from "./src/_string.js";
-
-const Helper = {
-  _Array,
-  _Crypto,
-  _Css,
-  _Dom,
-  _Error,
-  _Fetch,
-  _Is,
-  _Number,
-  _Object,
-  _String,
-};
-export {Helper as default}
+export * as _Array from "./src/_array.js";
+export * as _Crypto from "./src/_crypto.js";
+export * as _Css from "./src/_css.js";
+export * as _Dom from "./src/_dom.js";
+export * as _Error from "./src/_error.js";
+export * as _Fetch from "./src/_fetch.js";
+export * as _Func from "./src/_func.js";
+export {default as _Is} from "./src/_is.js";
+export * as _Number from "./src/_number.js";
+export * as _Object from "./src/_object.js";
+export * as _String from "./src/_string.js";
